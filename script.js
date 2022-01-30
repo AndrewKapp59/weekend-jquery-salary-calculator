3ef524d047dd782bde9e09510ee8759a5ff065ca
Andrew Kapp <dkapp59@gmail.com>
2022-01-30 14:31:55 -0600

    figured out how combine a few functions, working on getting total monthly to work

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ $(document).ready(readyNow);
 console.log('JS Working');
 
 const maxEmployees = 10;
-let employeeArray = [];
 
 function readyNow(){
 $('#submitButton').on('click', grabEmployeeInfo);
-$('#submitButton').on('click', addNewEmployee);
+// $('#submitButton').on('click', addNewEmployee);
 $('#submitButton').on('click', totalMonthly);
 $('#newEmployee').on('click', '#getRid', getRid);
 }
@@ -26,44 +25,130 @@ function grabEmployeeInfo() {
   let addID = $('#id').val();
   let addTitle = $('#title').val();
   let addAnnualSalary = $('#annualSalary').val();
-
-  if ( employeeArray.length < maxEmployees) {
-    newEmployee(addFirst, addLast, addID, addTitle, addAnnualSalary)
-  }
-}
-
-// Adds those values to an object and pushes the object to an array of objects
-function newEmployee(addFirst, addLast, addID, addTitle, addAnnualSalary){
-  console.log('New Employee Info:', addFirst, addLast, addID, addTitle, addAnnualSalary );
-  const newEmployeeObject = {
+  
+  //creates object
+  const nep = {
     firstName: addFirst,
     lastName: addLast,
     id: addID,
     title: addTitle,
     annualSalary: addAnnualSalary
   }
-  employeeArray.push(newEmployeeObject)
-  console.log(employeeArray);
+
+  //appends object info to DOM
+  $('#newEmployee').append(
+    `<tr>
+      <td>${nep.firstName}</td>
+      <td>${nep.lastName}</td>
+      <td>${nep.id}</td>
+      <td>${nep.title}</td>
+      <td>${nep.annualSalary}</td>
+      <td><button id='getRid'>Delete</button></td>
+     </tr>`);
+
+  
 }
+
+function totalMonthly() {
+  let total = 0;
+  for (let i of employeeArray) {
+    total += Number(i.annualSalary);
+  }
+  if (total < 20000) {
+    let el = $('#mt');
+    el.empty();
+    el.append(`<h4> Total Monthly: ` + Math.round(total/12) + `</h4>`)
+    $('.mt').addClass('white')
+  }
+  if (total >= 20000) {
+    let el = $('#mt');
+    el.empty();
+    el.append(`<h4> Total Monthly: ` + Math.round(total/12) + `</h4>`)
+    $('.mt').addClass('red')
+  }
+  console.log('Total Monthly working');
+}
+
+
+// Adds those values to an object and inputs them into the addNewEmployee function
+// function newEmployee(addFirst, addLast, addID, addTitle, addAnnualSalary){
+//   console.log('New Employee Info:', addFirst, addLast, addID, addTitle, addAnnualSalary );
+//   const nep = {
+//     firstName: addFirst,
+//     lastName: addLast,
+//     id: addID,
+//     title: addTitle,
+//     annualSalary: addAnnualSalary
+//   }
+//  addNewEmployee(nep.firstName, nep.lastName, nep.id, nep.title, nep.annualSalary)
+// }
+
+// function addNewEmployee(first, last, id, title, salary) {
+//   $('#newEmployee').append(
+//     `<tr>
+//       <td>${first}</td>
+//       <td>${last}</td>
+//       <td>${id}</td>
+//       <td>${title}</td>
+//       <td>${salary}</td>
+//       <td><button id='getRid'>Delete</button></td>
+//      </tr>`);
+//   console.log('Append is working');
+// }
 
 // Pulls the info from the objects in the employeeArray and appends them to the table body
 
-function addNewEmployee() {
-  let el = $('#newEmployee')
-  el.empty();
-  for ( i of employeeArray) {
-    $('#newEmployee').append(
-   `<tr>
-     <td>${i.firstName}</td>
-     <td>${i.lastName}</td>
-     <td>${i.id}</td>
-     <td>${i.title}</td>
-     <td>${i.annualSalary}</td>
-     <td><button id='getRid'>Delete</button></td>
-    </tr>`);
-  }
-  console.log('Append is working');
-}
+// function addNewEmployee() {
+//   let el = $('#newEmployee')
+//   el.empty();
+//   for ( i of employeeArray) {
+//     $('#newEmployee').append(
+//    `<tr>
+//      <td>${i.firstName}</td>
+//      <td>${i.lastName}</td>
+//      <td>${i.id}</td>
+//      <td>${i.title}</td>
+//      <td>${i.annualSalary}</td>
+//      <td><button id='getRid'>Delete</button></td>
+//     </tr>`);
+//   }
+//   console.log('Append is working');
+// }
+
+
+
+// Adds those values to an object and pushes the object to an array of objects
+// function newEmployee(addFirst, addLast, addID, addTitle, addAnnualSalary){
+//   console.log('New Employee Info:', addFirst, addLast, addID, addTitle, addAnnualSalary );
+//   const newEmployeeObject = {
+//     firstName: addFirst,
+//     lastName: addLast,
+//     id: addID,
+//     title: addTitle,
+//     annualSalary: addAnnualSalary
+//   }
+//   employeeArray.push(newEmployeeObject)
+//   console.log(employeeArray);
+// }
+
+// // Pulls the info from the objects in the employeeArray and appends them to the table body
+
+// function addNewEmployee() {
+//   let el = $('#newEmployee')
+//   el.empty();
+//   for ( i of employeeArray) {
+//     $('#newEmployee').append(
+//    `<tr>
+//      <td>${i.firstName}</td>
+//      <td>${i.lastName}</td>
+//      <td>${i.id}</td>
+//      <td>${i.title}</td>
+//      <td>${i.annualSalary}</td>
+//      <td><button id='getRid'>Delete</button></td>
+//     </tr>`);
+//   }
+//   console.log('Append is working');
+// }
 
 // function addNewEmployee() {
 //   let el = $('#newEmployee')
