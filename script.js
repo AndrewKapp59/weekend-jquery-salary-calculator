4e40141a64cb784882c7ba6e79afa6a98665d871
Andrew Kapp <dkapp59@gmail.com>
2022-01-30 14:50:01 -0600

    saving before attempting stretch

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ const maxEmployees = 10;
 employeeSalaries = []
 
 function readyNow(){
-$('#submitButton').on('click', grabEmployeeInfo);
-// $('#submitButton').on('click', addNewEmployee);
+$('#submitButton').on('click', NewEmployee);
 $('#submitButton').on('click', totalMonthly);
 $('#newEmployee').on('click', '#getRid', getRid);
 }
 
-// Takes input from user, turns them into values to be added to an object
-function grabEmployeeInfo() {  
+
+function NewEmployee() {  
+  
+  // Takes input from user, turns them into values to be added to an object  
   let addFirst = $('#firstName').val();
   let addLast = $('#lastName').val();
   let addID = $('#id').val();
   let addTitle = $('#title').val();
   let addAnnualSalary = $('#annualSalary').val();
- 
-  //creates object
-  const nep = {
+  
+  //creates a new employee object i.e. neo
+  const neo = {
     firstName: addFirst,
     lastName: addLast,
     id: addID,
@@ -29,20 +30,23 @@ function grabEmployeeInfo() {
     annualSalary: addAnnualSalary
   }
 
-  employeeSalaries.push(nep.annualSalary)
+  //pushes annual salary to an array for total monthly 
+  employeeSalaries.push(neo.annualSalary)
 
-  //appends object info to DOM
+  //appends neo info to DOM
   $('#newEmployee').append(
     `<tr>
-      <td>${nep.firstName}</td>
-      <td>${nep.lastName}</td>
-      <td>${nep.id}</td>
-      <td>${nep.title}</td>
-      <td>${nep.annualSalary}</td>
+      <td>${neo.firstName}</td>
+      <td>${neo.lastName}</td>
+      <td>${neo.id}</td>
+      <td>${neo.title}</td>
+      <td>${neo.annualSalary}</td>
       <td><button id='getRid'>Delete</button></td>
      </tr>`);
 }
 
+// does the math to figure out the total monthly and appends it to the DOM
+// adds a red background if total is > 20000
 function totalMonthly() {
   let total = 0;
   for (let i of employeeSalaries) {
@@ -51,13 +55,13 @@ function totalMonthly() {
   if (total < 20000) {
     let el = $('#mt');
     el.empty();
-    el.append(`<h4> Total Monthly: ` + Math.round(total/12) + `</h4>`)
+    el.append(`<h4> Total Monthly: $` + Math.round(total/12) + `</h4>`)
     $('.mt').addClass('white')
   }
   if (total >= 20000) {
     let el = $('#mt');
     el.empty();
-    el.append(`<h4> Total Monthly: ` + Math.round(total/12) + `</h4>`)
+    el.append(`<h4> Total Monthly: $` + Math.round(total/12) + `</h4>`)
     $('.mt').addClass('red')
   }
   console.log('Total Monthly working');
@@ -70,127 +74,6 @@ function getRid() {
   console.log('Get Rid is running');
 }
 
-// Adds those values to an object and inputs them into the addNewEmployee function
-// function newEmployee(addFirst, addLast, addID, addTitle, addAnnualSalary){
-//   console.log('New Employee Info:', addFirst, addLast, addID, addTitle, addAnnualSalary );
-//   const nep = {
-//     firstName: addFirst,
-//     lastName: addLast,
-//     id: addID,
-//     title: addTitle,
-//     annualSalary: addAnnualSalary
-//   }
-//  addNewEmployee(nep.firstName, nep.lastName, nep.id, nep.title, nep.annualSalary)
-// }
-
-// function addNewEmployee(first, last, id, title, salary) {
-//   $('#newEmployee').append(
-//     `<tr>
-//       <td>${first}</td>
-//       <td>${last}</td>
-//       <td>${id}</td>
-//       <td>${title}</td>
-//       <td>${salary}</td>
-//       <td><button id='getRid'>Delete</button></td>
-//      </tr>`);
-//   console.log('Append is working');
-// }
-
-// Pulls the info from the objects in the employeeArray and appends them to the table body
-
-// function addNewEmployee() {
-//   let el = $('#newEmployee')
-//   el.empty();
-//   for ( i of employeeArray) {
-//     $('#newEmployee').append(
-//    `<tr>
-//      <td>${i.firstName}</td>
-//      <td>${i.lastName}</td>
-//      <td>${i.id}</td>
-//      <td>${i.title}</td>
-//      <td>${i.annualSalary}</td>
-//      <td><button id='getRid'>Delete</button></td>
-//     </tr>`);
-//   }
-//   console.log('Append is working');
-// }
-
-
-
-// Adds those values to an object and pushes the object to an array of objects
-// function newEmployee(addFirst, addLast, addID, addTitle, addAnnualSalary){
-//   console.log('New Employee Info:', addFirst, addLast, addID, addTitle, addAnnualSalary );
-//   const newEmployeeObject = {
-//     firstName: addFirst,
-//     lastName: addLast,
-//     id: addID,
-//     title: addTitle,
-//     annualSalary: addAnnualSalary
-//   }
-//   employeeArray.push(newEmployeeObject)
-//   console.log(employeeArray);
-// }
-
-// // Pulls the info from the objects in the employeeArray and appends them to the table body
-
-// function addNewEmployee() {
-//   let el = $('#newEmployee')
-//   el.empty();
-//   for ( i of employeeArray) {
-//     $('#newEmployee').append(
-//    `<tr>
-//      <td>${i.firstName}</td>
-//      <td>${i.lastName}</td>
-//      <td>${i.id}</td>
-//      <td>${i.title}</td>
-//      <td>${i.annualSalary}</td>
-//      <td><button id='getRid'>Delete</button></td>
-//     </tr>`);
-//   }
-//   console.log('Append is working');
-// }
-
-// function addNewEmployee() {
-//   let el = $('#newEmployee')
-//   el.empty();
-//   for ( i of employeeArray) {
-//     el.append([
-//       `<tr class = 'bloc'>`,
-//        `<td>` + i.firstName + `</td>`,
-//        `<td>` + i.lastName + `</td>`,
-//        `<td>` + i.id + `</td>`,
-//        `<td>` + i.title + `</td>`,
-//        `<td>` + i.annualSalary + `</td>`,
-//        `<button id = 'getRid'>Delete</button>`,
-//       `</tr>`
-//     ])
-//   }
-//   console.log('Append is working');
-// }
- 
-// loop through the array and add up all the annualSalaries and appends them to the Dom
-// also the if monthly total if > 20,000 and appends monthly totals class and turns it's 
-// background red
-
-// function totalMonthly() {
-//   let total = 0;
-//   for (let i of employeeArray) {
-//     total += Number(i.annualSalary);
-//   }
-//   if (total < 20000) {
-//     let el = $('#mt');
-//     el.empty();
-//     el.append(`<h4> Total Monthly: ` + Math.round(total/12) + `</h4>`)
-//     $('.mt').addClass('white')
-//   }
-//   if (total >= 20000) {
-//     let el = $('#mt');
-//     el.empty();
-//     el.append(`<h4> Total Monthly: ` + Math.round(total/12) + `</h4>`)
-//     $('.mt').addClass('red')
-//   }
-//   console.log('Total Monthly working');
-// }
 
 
 
